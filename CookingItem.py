c3c235ceb51ddede67de6fd4390b8c5ce9e6880c
Eric Fruchter <ericpfruchter@gmail.com>
2019-11-10 14:42:26 -0500

    Added functionality to print what's in the pantry and to add on to a item alrady in the pantry

--- CookingItem.py
+++ CookingItem.py
@@ -15,9 +15,15 @@ class CookingItem:
         self.amount -= amount_used
         print("You selected to use {au} of your {it}. You have {al}".format(au=amount_used, it=self.item, al=self.amount))
     
-    def add_amnt(self, amount_bought):
-        self.amount += amount_bought
-        print("You selected to add {ab} of your {it}. You have {al}".format(ab=amount_bought, it=self.item, al=self.amount))    
+    def add_amnt(self):
+        qty_to_add = ""
+        while len(qty_to_add) == 0:
+            qty_to_add = input("How much {0} would you like to add to your pantry?:".format(self.item))
+        self.amount += int(qty_to_add)
+        print("You selected to add {ab} of your {it}. You have {al}".format(ab=qty_to_add, it=self.item, al=self.amount))    
  
     def enough_left(self, amount_needed):
         return self.amount >= amount_needed
+    
+    def get_item(self):
+        return self.item, self.amount
